a517ad79c61c037892aee7b3c49115c3914f287f
Sameer Chirumamilla <schirumamilla@Sameers-MacBook-Pro.local>
2017-10-22 13:50:58 -0700

    Adding File_Processing

---
+++ File_Processing.py
@@ -0,0 +1,27 @@
+filename='~/write_python'
+#for line in open(filename):
+
+# read a file
+"""
+with open(filename) as file_handler:
+	lines = file_handler.readlines()
+	for line in lines:
+		print line
+"""
+
+# write and append to a file
+"""
+with open(filename,'w') as file_handler:
+	lines = file_handler.write("write a file into python \n")
+with open(filename,'a') as file_appender:
+	file_appender.write("Appending a new line into python file \n")
+"""
+try:
+	#filename = '/var/log/notthere'
+	for line in open(filename):
+		print line
+except IOError:
+	print "File doesn't exist"
+else:
+	print "Done processing the file"
+
