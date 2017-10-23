22eeac062476ad3119674466dcca0f56c5cd4366
Sameer Chirumamilla <schirumamilla@Sameers-MacBook-Pro.local>
2017-10-22 21:39:07 -0700

    Renamed

---
+++ Python_101/Basics.py
@@ -0,0 +1,19 @@
+# Python Variable Basics and Numeric Variables
+
+# Strings
+
+# Standard Modules and Libraries
+
+# If Statements
+
+# Loops
+
+# Lists
+
+# Dictornaries or Dicts
+
+# Tuples and sets
+
+# Functions
+
+# Exceptions
